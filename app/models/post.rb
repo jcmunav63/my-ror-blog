a7445e18b787a2efae17568ad638932dc7c1a1bc
jcmunav63 <jcmunav63@gmail.com>
2024-01-10 14:29:01 -0600

    Create validations inside User and Post models. Adapt Readme file.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
