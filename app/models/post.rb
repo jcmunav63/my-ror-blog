adde50d8d3a9bc79d5332b829a5c75dd9f0eab28
jcmunav63 <jcmunav63@gmail.com>
2024-01-09 21:21:11 -0600

    Edit User, Post, Comment & Like models to implement data processing methods.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  def update_user_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
+
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  def three_most_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
