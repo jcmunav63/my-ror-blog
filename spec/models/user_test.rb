488d82afd2f5a7145ec13ab3090e9cf48c3fc74a
jcmunav63 <jcmunav63@gmail.com>
2024-01-11 10:39:08 -0600

    create folder spec. Add tests for User and Post models.

---
+++ spec/models/user_test.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { should validate_presence_of(:name) }
+  it { should validate_numericality_of(:posts_counter).only_integer.is_greater_than_or_equal_to(0) }
+
+  it { should have_many(:posts).dependent(:destroy) }
+end
