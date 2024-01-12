a21fe33f29c190247d8e1937c14e230ad1fe6d9f
jcmunav63 <jcmunav63@gmail.com>
2024-01-11 18:35:34 -0600

    Update Comment tests.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'The Comment model tests' do
-  it 'should have valid user, post and update comments_counter in post object' do
-    user = User.create(id: 13, name: 'Elo', photo_url: 'https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014_640.jpg',
-                       bio: 'Computer Eng', posts_counter: 0)
-    post = Post.create(author_id: user.id, title: 'Hello', text: 'This is my first post', comments_counter: 0,
-                       likes_counter: 0)
-    comment = Comment.create(post:, user:, text: 'Hi Elo!')
+  it 'Updates the post\'s comment_counter when creating a new comment' do
+    user1 = User.create(name: 'John Denver', photo: 'https://cdn.pixabay.com/photo/2005/03/22/17/15/songs-76388_320.jpg',
+                        bio: 'American Professor', posts_counter: 0)
+    post1 = Post.create(author_id: user1.id, title: 'Post #5', text: 'A post about computer programming.',
+                        comments_counter: 0, likes_counter: 0)
+    comment1 = Comment.create(post: post1, user: user1, text: 'This is a comment for this post!')
 
-    expect(comment).to be_valid
-    expect(post.comments_counter).to eq(1)
+    expect(comment1).to be_valid
+    expect(post1.comments_counter).to eq(1)
   end
 end
