540c2bc96c5477d5ce6fff0c8e7ad6d5cc8a15cc
jcmunav63 <jcmunav63@gmail.com>
2024-01-23 19:29:17 -0600

    Edit users_controller to correct a linters error.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.paginate(page: params[:page], per_page: 5)
-    @show_all_posts_button = @user.posts_counter > 0
+    @show_all_posts_button = @user.posts_counter.positive?
   end
 end
