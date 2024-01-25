42beab89c8227c68a543acb23dc28733ca541c7c
jcmunav63 <jcmunav63@gmail.com>
2024-01-25 12:14:37 -0600

    Add form for new Post, configure routes, and add link to create the post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,24 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to user_post_path(current_user, @post), notice: 'Post was successfully created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
