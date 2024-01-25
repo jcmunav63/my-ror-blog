01f657b063ea995677f749104db65cf0643679bc
jcmunav63 <jcmunav63@gmail.com>
2024-01-25 13:10:20 -0600

    Edit users and posts controllers, as well as the posts#index and posts#new views, to correct errors.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_current_user, only: [:index, :new, :create]
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.paginate(page: params[:page], per_page: 2)
@@ -9,14 +11,14 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = current_user.posts.build
+    @post = @current_user.posts.build
   end
 
   def create
-    @post = current_user.posts.build(post_params)
+    @post = @current_user.posts.build(post_params)
 
     if @post.save
-      redirect_to user_post_path(current_user, @post), notice: 'Post was successfully created!'
+      redirect_to user_post_path(@current_user, @post), notice: 'Post was successfully created!'
     else
       render :new
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_current_user, only: [:index, :show]
+
   def index
     @users = User.all
   end
@@ -8,4 +10,10 @@ class UsersController < ApplicationController
     @posts = @user.posts.paginate(page: params[:page], per_page: 5)
     @show_all_posts_button = @user.posts_counter.positive?
   end
+
+  # private
+
+  # def set_current_user
+  #   @current_user ||= User.first
+  # end
 end
