db31633184280d9a633a32952f358dcccb91fdfc
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-02 11:32:04 -0600

    Update: routes.

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   root to: 'users#index'
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:new, :create, :index, :show] do
+    resources :posts, only: [:new, :create, :index, :show, :destroy] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:create, :destroy]
     end
