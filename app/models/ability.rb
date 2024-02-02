65c4c7875a99ab7329295d7c9bb9850a43513aef
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-02 11:31:40 -0600

    Feat: add abilities.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,12 +6,14 @@ class Ability
   def initialize(user)
     user ||= User.new  # guest user (not logged in)
     
-    unless user.admin?
+    if user.admin?
       can :manage, :all
     else
       can :read, :all
-      can :destroy, Post, author_id: user.id # Check user_id / :update, 
-      can :destroy, Comment, user_id: user.id # :update, 
+      can :create, Post
+      can :create, Comment
+      can :destroy, Post, author_id: user.id # Check user_id / :update,
+      can :destroy, Comment, user_id: user.id # :update,
     end
   end
 end
