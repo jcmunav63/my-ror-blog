256b412474a9e3f2ae90d0f023f79447ab1721e2
jcmunav63 <jcmunav63@gmail.com>
2024-02-03 09:16:25 -0600

    Edit posts_index file to correct comments generation.

--- spec/system/posts_index_integration_spec.rb
+++ spec/system/posts_index_integration_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Posts Index Page', type: :system do
   before do
     @user = create(:user)
     @posts = create_list(:post, 5, author: @user)
-    @posts.each { |post| create_list(:comment, 3, post: post, user: @user) }
+    @posts.each { |post| create_list(:comment, 3, post: post, author: @user) }
 
     visit user_posts_path(@user)
 
