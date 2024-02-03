bbac5bd4ca0a30742a9d07aa8aa027a23b57c000
jcmunav63 <jcmunav63@gmail.com>
2024-02-03 10:39:33 -0600

    Add posts_show page tests.

--- spec/system/posts_index_integration_spec.rb
+++ spec/system/posts_index_integration_spec.rb
@@ -1,28 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts Index Page', type: :system do
-  # let(:user) { User.create(name: 'John Carter', posts_counter: 3, photo: '/assets/users/user1.PNG') }
-
   before do
     @user = create(:user)
     @posts = create_list(:post, 5, author: @user)
     @posts.each { |post| create_list(:comment, 3, post: post, author: @user) }
 
     visit user_posts_path(@user)
-
-    # Create user's posts and comments for testing
-    # p = Post.create(author: user, title: 'Post 1', text: 'Lorem ipsum 1')
-    # q = Post.create(author: user, title: 'Post 2', text: 'Lorem ipsum 2')
-    # r = Post.create(author: user, title: 'Post 3', text: 'Lorem ipsum 3')
-
-    # Comment.create(post: p, author: user, text: 'Comment 1 for Post 1')
-    # Comment.create(post: p, author: user, text: 'Comment 2 for Post 1')
-    # Comment.create(post: q, author: user, text: 'Comment 1 for Post 2')
-    # Comment.create(post: q, author: user, text: 'Comment 2 for Post 2')
-    # Comment.create(post: r, author: user, text: 'Comment 1 for Post 3')
-    # Comment.create(post: r, author: user, text: 'Comment 2 for Post 3')
-
-    # visit user_posts_path(user)
   end
 
   it 'displays the user\'s profile picture' do
@@ -41,26 +25,12 @@ RSpec.describe 'Posts Index Page', type: :system do
     @posts.each do |post|
       expect(page).to have_content(post.title)
     end
-    # user.posts.each do |post|
-    #   puts "Checking for title: #{post.title}"
-    #   within(".post-line h3", text: post.title) do
-    #     expect(page).to have_content("Post #{post.id}: #{post.title}")
-    #   end
-    #   # expect(page).to have_css(".post-line h3", text: "Post #{post.id}: #{post.title}")
-    # end
   end
 
   it 'displays each post\'s text (body)' do
     @posts.each do |post|
       expect(page).to have_content(post.text)
     end
-    # user.posts.each do |post|
-    #   puts "Checking for text: #{post.text}"
-    #   within(".post-line p", text: post.text) do
-    #     expect(page).to have_content(post.text)
-    #   end
-    #   # expect(page).to have_css(".post-line p", text: post.text)
-    # end
   end
 
   it 'displays each post\'s five most recent comments' do
@@ -69,54 +39,26 @@ RSpec.describe 'Posts Index Page', type: :system do
         expect(page).to have_content(comment.text)
       end
     end
-    # user.posts.each do |post|
-    #   within(".post-line h3", text: "Post #{post.id}: #{post.title}") do
-    #     within(".comments") do
-    #       post.five_most_recent_comments.each do |comment|
-    #         expect(page).to have_content("#{comment.author.name}: #{comment.text}")
-    #       end
-    #     end
-    #   end
-    # end
   end
-
-  # within(".post-container", text: "Post #{post.id}: #{post.title}") do
-  #   post.five_most_recent_comments.each do |comment|
-  #   expect(page).to have_content("#{comment.author.name}: #{comment.text}")
-  # end
-  
 
   it 'displays each post\'s comments_counter' do
     @posts.each do |post|
       expect(page).to have_content("Comments: #{post.comments_counter}")
     end
-    # user.posts.each do |post|
-    #   expect(page).to have_content("Comments: #{post.comments_counter}")
-    # end
   end
 
   it 'displays each post\'s likes_counter' do
     @posts.each do |post|
       expect(page).to have_content("Likes: #{post.likes_counter}")
     end
-    # user.posts.each do |post|
-    #   expect(page).to have_content("Likes: #{post.likes_counter}")
-    # end
   end
 
   it 'displays a link to view a post\'s show page' do
     @posts.first
-    # first('a').click
     expect(page).to have_link('Read More', href: user_post_path(@user, @posts.first))
   end
 
   it 'displays a link to create a new post' do
     expect(page).to have_link('New Post', href: new_user_post_path(@user))
   end
-
-  # private
-
-  # def truncate(text, options = {})
-  #   ActionController::Base.helpers.truncate(text, options)
-  # end
 end
